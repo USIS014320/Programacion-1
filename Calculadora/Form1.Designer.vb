330216fdb047ffbdb4c16272643b363e35cc3c79
Brandyn Eraldo González Gómez <68960634+USIS014320@users.noreply.github.com>
2020-09-11 11:26:13 -0600

    Resolucion de la tarea de los conversores.

--- Calculadora/Form1.Designer.vb
+++ Calculadora/Form1.Designer.vb
@@ -22,41 +22,48 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnCalcular = New System.Windows.Forms.Button()
-        Me.lblNúmero = New System.Windows.Forms.Label()
-        Me.txtNúmero = New System.Windows.Forms.TextBox()
+        Me.btnConvertir = New System.Windows.Forms.Button()
+        Me.lblValor = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
         Me.txtRespuesta = New System.Windows.Forms.TextBox()
         Me.lblres = New System.Windows.Forms.Label()
+        Me.cboTipo = New System.Windows.Forms.ComboBox()
+        Me.lblTipo = New System.Windows.Forms.Label()
+        Me.lblDe = New System.Windows.Forms.Label()
+        Me.cboDe = New System.Windows.Forms.ComboBox()
+        Me.lblA = New System.Windows.Forms.Label()
+        Me.cboA = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
-        'btnCalcular
+        'btnConvertir
         '
-        Me.btnCalcular.Location = New System.Drawing.Point(190, 322)
-        Me.btnCalcular.Name = "btnCalcular"
-        Me.btnCalcular.Size = New System.Drawing.Size(215, 69)
-        Me.btnCalcular.TabIndex = 0
-        Me.btnCalcular.Text = "CALCULAR"
-        Me.btnCalcular.UseVisualStyleBackColor = True
+        Me.btnConvertir.Location = New System.Drawing.Point(205, 340)
+        Me.btnConvertir.Name = "btnConvertir"
+        Me.btnConvertir.Size = New System.Drawing.Size(201, 54)
+        Me.btnConvertir.TabIndex = 0
+        Me.btnConvertir.Text = "CONVERTIR"
+        Me.btnConvertir.UseVisualStyleBackColor = True
         '
-        'lblNúmero
+        'lblValor
         '
-        Me.lblNúmero.AutoSize = True
-        Me.lblNúmero.Location = New System.Drawing.Point(261, 63)
-        Me.lblNúmero.Name = "lblNúmero"
-        Me.lblNúmero.Size = New System.Drawing.Size(69, 17)
-        Me.lblNúmero.TabIndex = 1
-        Me.lblNúmero.Text = "NUMERO"
+        Me.lblValor.AutoSize = True
+        Me.lblValor.Location = New System.Drawing.Point(271, 31)
+        Me.lblValor.Name = "lblValor"
+        Me.lblValor.Size = New System.Drawing.Size(55, 17)
+        Me.lblValor.TabIndex = 1
+        Me.lblValor.Text = "VALOR"
         '
-        'txtNúmero
+        'txtCantidad
         '
-        Me.txtNúmero.Location = New System.Drawing.Point(243, 83)
-        Me.txtNúmero.Name = "txtNúmero"
-        Me.txtNúmero.Size = New System.Drawing.Size(100, 22)
-        Me.txtNúmero.TabIndex = 2
+        Me.txtCantidad.Location = New System.Drawing.Point(253, 51)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 22)
+        Me.txtCantidad.TabIndex = 2
+        Me.txtCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtRespuesta
         '
-        Me.txtRespuesta.Location = New System.Drawing.Point(204, 194)
+        Me.txtRespuesta.Location = New System.Drawing.Point(205, 286)
         Me.txtRespuesta.Name = "txtRespuesta"
         Me.txtRespuesta.Size = New System.Drawing.Size(201, 22)
         Me.txtRespuesta.TabIndex = 4
@@ -65,11 +72,67 @@ Partial Class Form1
         'lblres
         '
         Me.lblres.AutoSize = True
-        Me.lblres.Location = New System.Drawing.Point(261, 174)
+        Me.lblres.Location = New System.Drawing.Point(262, 266)
         Me.lblres.Name = "lblres"
         Me.lblres.Size = New System.Drawing.Size(91, 17)
         Me.lblres.TabIndex = 5
         Me.lblres.Text = "RESPUESTA"
+        '
+        'cboTipo
+        '
+        Me.cboTipo.BackColor = System.Drawing.Color.White
+        Me.cboTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboTipo.FormattingEnabled = True
+        Me.cboTipo.Items.AddRange(New Object() {"Moneda", "Longitud", "Masa", "Almacenamiento", "Tiempo"})
+        Me.cboTipo.Location = New System.Drawing.Point(410, 51)
+        Me.cboTipo.Name = "cboTipo"
+        Me.cboTipo.Size = New System.Drawing.Size(121, 24)
+        Me.cboTipo.TabIndex = 6
+        '
+        'lblTipo
+        '
+        Me.lblTipo.AutoSize = True
+        Me.lblTipo.Location = New System.Drawing.Point(449, 31)
+        Me.lblTipo.Name = "lblTipo"
+        Me.lblTipo.Size = New System.Drawing.Size(40, 17)
+        Me.lblTipo.TabIndex = 7
+        Me.lblTipo.Text = "TIPO"
+        '
+        'lblDe
+        '
+        Me.lblDe.AutoSize = True
+        Me.lblDe.Location = New System.Drawing.Point(102, 129)
+        Me.lblDe.Name = "lblDe"
+        Me.lblDe.Size = New System.Drawing.Size(27, 17)
+        Me.lblDe.TabIndex = 9
+        Me.lblDe.Text = "DE"
+        '
+        'cboDe
+        '
+        Me.cboDe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboDe.FormattingEnabled = True
+        Me.cboDe.Location = New System.Drawing.Point(56, 149)
+        Me.cboDe.Name = "cboDe"
+        Me.cboDe.Size = New System.Drawing.Size(121, 24)
+        Me.cboDe.TabIndex = 8
+        '
+        'lblA
+        '
+        Me.lblA.AutoSize = True
+        Me.lblA.Location = New System.Drawing.Point(449, 129)
+        Me.lblA.Name = "lblA"
+        Me.lblA.Size = New System.Drawing.Size(17, 17)
+        Me.lblA.TabIndex = 11
+        Me.lblA.Text = "A"
+        '
+        'cboA
+        '
+        Me.cboA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboA.FormattingEnabled = True
+        Me.cboA.Location = New System.Drawing.Point(391, 149)
+        Me.cboA.Name = "cboA"
+        Me.cboA.Size = New System.Drawing.Size(121, 24)
+        Me.cboA.TabIndex = 10
         '
         'Form1
         '
@@ -77,23 +140,35 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.OrangeRed
         Me.ClientSize = New System.Drawing.Size(590, 454)
+        Me.Controls.Add(Me.lblA)
+        Me.Controls.Add(Me.cboA)
+        Me.Controls.Add(Me.lblDe)
+        Me.Controls.Add(Me.cboDe)
+        Me.Controls.Add(Me.lblTipo)
+        Me.Controls.Add(Me.cboTipo)
         Me.Controls.Add(Me.lblres)
         Me.Controls.Add(Me.txtRespuesta)
-        Me.Controls.Add(Me.txtNúmero)
-        Me.Controls.Add(Me.lblNúmero)
-        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.lblValor)
+        Me.Controls.Add(Me.btnConvertir)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "DECIMALES A ROMANOS"
+        Me.Text = "CONVERSORES"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents btnCalcular As Button
-    Friend WithEvents lblNúmero As Label
-    Friend WithEvents txtNúmero As TextBox
+    Friend WithEvents btnConvertir As Button
+    Friend WithEvents lblValor As Label
+    Friend WithEvents txtCantidad As TextBox
     Friend WithEvents txtRespuesta As TextBox
     Friend WithEvents lblres As Label
+    Friend WithEvents cboTipo As ComboBox
+    Friend WithEvents lblTipo As Label
+    Friend WithEvents lblDe As Label
+    Friend WithEvents cboDe As ComboBox
+    Friend WithEvents lblA As Label
+    Friend WithEvents cboA As ComboBox
 End Class
--- Calculadora/Form1.vb
+++ Calculadora/Form1.vb
@@ -1,27 +1,38 @@
 ﻿Public Class Form1
-    Dim objromanos = New conversor_romanos
+    Dim objConversor = New conversores()
+    Private Sub cbotipo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbotipo.SelectedIndexChanged
+        cbode.Items.Clear()
+        cbode.Text = ""
+        cbode.Items.AddRange(objConversor.etiquetas(cbotipo.SelectedIndex))
 
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        txtRespuesta.Text = objromanos.convertir(txtNúmero.Text)
+        cboa.Items.Clear()
+        cboa.Text = ""
+        cboa.Items.AddRange(objConversor.etiquetas(cbotipo.SelectedIndex))
     End Sub
-End Class
 
-Class conversor_romanos
-    Dim romanos()() As String = {
-        New String() {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
-        New String() {"", "X", "XX", "XXX", "XL", "LV", "LXX", "LXXX", "XC"},
-        New String() {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+        txtRespuesta.Text = objConversor.convertir(cboTipo.SelectedIndex, cboDe.SelectedIndex, cboA.SelectedIndex, txtCantidad.Text).ToString() + " " + objConversor.etiquetas(cboTipo.SelectedIndex)(cboA.SelectedIndex)
+    End Sub
+
+    Class conversores
+        Public etiquetas()() As String = {
+            New String() {"Dolar", "Euro", "Quetzales", "Lempira", "Colon SV", "Cordoba", "Colon CR"},
+            New String() {"Metro", "CM", "Pulgadas", "Pies", "Varas", "Yardas", "Km", "Millas"},
+            New String() {"Libra", "Gramos", "Kilogramos", "Onzas", "Quintales", "Toneladas"},
+            New String() {"MB", "Bit", "Byte", "KB", "GB", "TB"},
+            New String() {"Hora", "Segundos", "Minutos", "Dias", "Semana", "Meses", "Años"}
+        }
+
+        Public valores()() As Double = {
+        New Double() {1, 0.85, 7.74, 24.8, 8.75, 34.6, 597.23},
+        New Double() {1, 100, 39.3701, 3.28084, 1.1963081929167, 1.09361, 0.001, 0.000621371},
+        New Double() {1, 453.592, 0.453592, 16, 0.01, 0.000453592},
+        New Double() {1, 8000000.0, 1000000.0, 1000, 0.001, 0.000001},
+        New Double() {1, 3600, 60, 0.0416667, 0.00595238, 0.00136986, 0.000114155}
     }
+        Public Function convertir(ByVal tipo As Int16, ByVal de As Int16, ByVal a As Int16, ByVal cantidad As Double)
+            Return valores(tipo)(a) / valores(tipo)(de) * cantidad
+        End Function
 
-    Dim u, d, c, temp As Integer
-
-    Public Function convertir(ByVal num As Integer)
-
-        u = num Mod 10
-        c = num \ 100
-        temp = num Mod 100
-        d = temp \ 10
-        Return romanos(2)(c) + romanos(1)(d) + romanos(0)(u)
-
-    End Function
+    End Class
 End Class
