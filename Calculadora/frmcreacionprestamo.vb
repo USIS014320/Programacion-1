4d557d55fb6672d184238c174ce034c09d50e330
Brandyn Eraldo Gonzalez Gomez <usis014320@ugb.edu.sv>
2020-11-22 06:06:51 -0600

    Avance 4 del formulario avanzado crear prestamo.

---
+++ Calculadora/frmcreacionprestamo.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmcreacionprestamo
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblCédulaPrest.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtTeléfonoPrest.TextChanged
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOcupacionPrest.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub lblPrest_Click(sender As Object, e As EventArgs) Handles lblPrest.Click
+
+    End Sub
+End Class
--- Calculadora/frmcrearprestamo.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class frmcrearprestamo
-
-End Class
