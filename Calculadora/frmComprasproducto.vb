2a3732a1fcc932f916db46047c43d953407a2eff
Denys Alexander Hernandez Maravilla <usis016320@ugb.edu.sv>
2020-12-11 11:35:34 -0600

    se esta mejorando el formulario

--- Calculadora/frmComprasproducto.vb
+++ Calculadora/frmComprasproducto.vb
@@ -1,12 +1,13 @@
-﻿Public Class frmComprasproducto
-    Private Sub ComprasBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ComprasBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.ComprasBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Bd_sigacDataSet)
+﻿Imports System.Data.SqlClient
+Public Class frmComprasproducto
+    Public _idCpro As Integer = 0
+    Private Sub frmComprasproductos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        actualizarDs()
+        totalizar()
 
     End Sub
 
-    Private Sub frmComprasproducto_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub actualizarDs()
         'TODO: esta línea de código carga datos en la tabla 'Bd_sigacDataSet.dcompras' Puede moverla o quitarla según sea necesario.
         Me.DcomprasTableAdapter.Fill(Me.Bd_sigacDataSet.dcompras)
         'TODO: esta línea de código carga datos en la tabla 'Bd_sigacDataSet.pagos' Puede moverla o quitarla según sea necesario.
@@ -30,26 +31,93 @@
 
             For i As Integer = 0 To nfilas
                 fila = DcomprasProductosDataGridView.Rows(i)
-
-
+                subtotal = Double.Parse(fila.Cells("cantidad").Value.ToString()) * Double.Parse(fila.Cells("precio").Value.ToString())
 
                 fila.Cells("subtotal").Value = subtotal.ToString()
                 sumas += subtotal
             Next
-            iva = If(IdTipofacturaComboBox.SelectedValue = 4, sumas * 0.13, 0)
+            iva = If(IdTipofacturaComboBox.SelectedValue = 2, sumas * 0.13, 0)
             total = sumas + iva
 
             lblrespuestasumacompra.Text = "$ " + Math.Round(sumas, 2).ToString()
             lblrespuestaivacompra.Text = "$ " + Math.Round(iva, 2).ToString()
             lblrespuestatotalcompra.Text = "$ " + Math.Round(total, 2).ToString()
 
+            lblnregistros.Text = (ComprasBindingSource.Position + 1) & " de " & ComprasBindingSource.Count
         Catch ex As Exception
-            MessageBox.Show("Error " + ex.Message)
+            'MessageBox.Show("Error " + ex.Message)
         End Try
     End Sub
 
-    Private Sub ComprasBindingNavigator_RefreshItems(sender As Object, e As EventArgs) Handles ComprasBindingNavigator.RefreshItems
+    Private Sub btnPrimero_Click(sender As Object, e As EventArgs) Handles btnPrimero.Click
+        ComprasBindingSource.MoveFirst()
         totalizar()
     End Sub
 
+    Private Sub btnAnterior_Click(sender As Object, e As EventArgs) Handles btnAnterior.Click
+        ComprasBindingSource.MovePrevious()
+        totalizar()
+    End Sub
+
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        ComprasBindingSource.MoveNext()
+        totalizar()
+    End Sub
+
+    Private Sub btnUltimo_Click(sender As Object, e As EventArgs) Handles btnUltimo.Click
+        ComprasBindingSource.MoveLast()
+        totalizar()
+    End Sub
+    Private Sub habdesh_controles(ByVal valor As Boolean)
+        NfacturaTextBox.ReadOnly = valor
+        IdProveedorComboBox.Enabled = Not valor
+        Fecha_vtaDateTimePicker.Enabled = Not valor
+        IdTipofacturaComboBox.Enabled = Not valor
+        IdPagoComboBox.Enabled = Not valor
+        DcomprasProductosDataGridView.ReadOnly = valor
+
+        grbdComprasProductos.Visible = Not valor
+        grbNavegacion.Visible = valor
+        btnAgregarProveedor.Visible = Not valor
+
+        btnEliminar.Enabled = valor
+        btnBuscar.Enabled = valor
+    End Sub
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        If btnAgregar.Text = "Nuevo" Then
+            btnAgregar.Text = "Guardar"
+            btnModificar.Text = "Cancelar"
+
+            habdesh_controles(False)
+            ComprasBindingSource.AddNew()
+
+
+
+        Else 'Guardar
+
+            habdesh_controles(True)
+            btnAgregar.Text = "Nuevo"
+            btnModificar.Text = "Modificar"
+        End If
+
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        If btnModificar.Text = "Modificar" Then
+            btnAgregar.Text = "Guardar"
+            btnModificar.Text = "Cancelar"
+
+            habdesh_controles(False)
+
+        Else 'cancelar
+            DcomprasProductosBindingSource.CancelEdit()
+            ComprasBindingSource.CancelEdit()
+            habdesh_controles(True)
+
+            btnAgregar.Text = "Nuevo"
+            btnModificar.Text = "Modificar"
+        End If
+
+    End Sub
 End Class
